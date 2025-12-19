b306da51e57e85530b593c12bf8bc64ab4f7f428
moonseungju <msj9965@gmail.com>
2025-12-19 16:43:36 +0900

    chore: initialize project(#1)

---
+++ VoiceBook/src/main/java/com/sj/voicebook/VoiceBookApplication.java
@@ -0,0 +1,13 @@
+package com.sj.voicebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceBookApplication.class, args);
+    }
+
+}
---
+++ VoiceBook/src/test/java/com/sj/voicebook/VoiceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sj.voicebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
