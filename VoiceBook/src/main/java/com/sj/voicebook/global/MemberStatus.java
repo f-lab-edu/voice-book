09db864e9c3c8286fc279aec35076a2dc060c5e5
moonseungju <msj9965@gmail.com>
2025-12-19 17:17:13 +0900

    Feat: Member의 상태를 활성, 비활성, 정지, 탈퇴로 구분하기 위한 MemberStatus 추가

---
+++ VoiceBook/src/main/java/com/sj/voicebook/global/MemberStatus.java
@@ -0,0 +1,18 @@
+package com.sj.voicebook.global;
+
+public enum MemberStatus {
+    ACTIVE("활성"),
+    INACTIVE("비활성"),
+    SUSPENDED("정지"),
+    WITHDRAWN("탈퇴");
+
+    private final String description;
+
+    MemberStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
