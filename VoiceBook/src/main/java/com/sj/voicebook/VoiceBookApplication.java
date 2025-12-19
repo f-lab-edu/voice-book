de2e7e5a50c63c43b793cbc085eb54f2867cf4e2
moonseungju <msj9965@gmail.com>
2025-12-19 20:12:02 +0900

    feat: Create_At, Update_At을 위해 @EnableJpaAuditing 추가 (#1)

--- VoiceBook/src/main/java/com/sj/voicebook/VoiceBookApplication.java
+++ VoiceBook/src/main/java/com/sj/voicebook/VoiceBookApplication.java
@@ -2,8 +2,10 @@ package com.sj.voicebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VoiceBookApplication {
 
     public static void main(String[] args) {
