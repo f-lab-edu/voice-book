759bc38a97f72f880765ff3bfe5fced713d585c3
moonseungju <msj9965@gmail.com>
2025-12-19 19:33:31 +0900

    Feat: Signup 인터페이스 구현 클래스 추가(#1)

---
+++ VoiceBook/src/main/java/com/sj/voicebook/member/service/impl/SignUpServiceImpl.java
@@ -0,0 +1,33 @@
+package com.sj.voicebook.member.service.impl;
+
+import com.sj.voicebook.member.domain.Member;
+import com.sj.voicebook.member.dto.application.CreateUserCommand;
+import com.sj.voicebook.member.repository.MemberRepository;
+import com.sj.voicebook.member.service.SignUpService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SignUpServiceImpl implements SignUpService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public String signUp(CreateUserCommand command) {
+        Member member = Member.from(command);
+        Member saveMember = memberRepository.save(member);
+        return saveMember.getNickname()+"님 회원가입을 환영합니다!";
+    }
+
+    @Override
+    public Boolean checkEmailDuplication(String email) {
+        return null;
+    }
+
+    @Override
+    public Boolean checkNicknameDuplication(String nickname) {
+        return null;
+    }
+}
