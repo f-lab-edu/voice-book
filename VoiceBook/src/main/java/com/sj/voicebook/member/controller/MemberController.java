278fb403d5ec54ca9482a95f0bd0c5e5a2c82e1e
moonseungju <msj9965@gmail.com>
2025-12-19 22:01:20 +0900

    feat: 프로필 이미지 조회 api 추가, 회원가입 시 프로필 이미지 저장하도록 수정 (#1)

--- VoiceBook/src/main/java/com/sj/voicebook/member/controller/MemberController.java
+++ VoiceBook/src/main/java/com/sj/voicebook/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.sj.voicebook.member.controller;
 import com.sj.voicebook.global.ApiResponse;
 import com.sj.voicebook.member.dto.api.CreateUserRequest;
 import com.sj.voicebook.member.dto.application.CreateUserCommand;
+import com.sj.voicebook.member.service.S3ImageService;
 import com.sj.voicebook.member.service.SignUpService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,23 +19,33 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/members")
 public class MemberController {
     private final SignUpService signUpService;
+    private final S3ImageService s3ImageService;
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<String>> signUp(
             @Valid @ModelAttribute CreateUserRequest request,
             @RequestParam(value = "profileImage", required = false) MultipartFile profileImage) {
 
+        String Key = s3ImageService.upload(profileImage);
+
         CreateUserCommand command = CreateUserCommand.builder()
                 .email(request.email())
                 .nickname(request.nickname())
                 .password(request.password())
-                .profileImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTj6s5jkrtoGn-lV4NKewUhS38dDDWRnmv-A&s")
+                .profileImage(Key)
                 .build();
 
         String result = signUpService.signUp(command);
         ApiResponse<String> response = ApiResponse.success(result, "회원가입이 완료되었습니다.");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/profile-image")
+    public ResponseEntity<ApiResponse<String>> viewImage(@RequestParam("key") String key) {
+        String url = s3ImageService.getPresignedUrl(key);
+        ApiResponse<String> response = ApiResponse.success(url, "프로필 이미지 조회에 성공했습니다.");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 
