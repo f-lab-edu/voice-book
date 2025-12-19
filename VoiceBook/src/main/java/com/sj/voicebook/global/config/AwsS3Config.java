4f67d740aaa7330f01728db159aff851a888a4e1
moonseungju <msj9965@gmail.com>
2025-12-19 22:00:23 +0900

    chore: aws 빈 등록 (#1)

---
+++ VoiceBook/src/main/java/com/sj/voicebook/global/config/AwsS3Config.java
@@ -0,0 +1,31 @@
+package com.sj.voicebook.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class AwsS3Config {
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    // Presigned URL을 생성하는 '발급기'를 빈으로 등록
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+}
