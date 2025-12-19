e762878fcc496589d37f2738f001fca996fe0cb6
moonseungju <msj9965@gmail.com>
2025-12-19 20:09:53 +0900

    fix: Service에서 비밀번호 암호화하도록 DTO 변경 (#1)

--- VoiceBook/src/main/java/com/sj/voicebook/member/dto/application/CreateUserCommand.java
+++ VoiceBook/src/main/java/com/sj/voicebook/member/dto/application/CreateUserCommand.java
@@ -1,17 +1,11 @@
 package com.sj.voicebook.member.dto.application;
 
-import com.sj.voicebook.global.MemberStatus;
-import com.sj.voicebook.global.Role;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.Builder;
 
-import java.time.LocalDateTime;
-
+@Builder
 public record CreateUserCommand(
         String email,
-        String encryptedPassword,
+        String password,
         String nickname,
         String profileImage) {
 
