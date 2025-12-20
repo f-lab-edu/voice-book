ef725771bcd0cb7dd3ed9893cf006f66c0256bcd
moonseungju <msj9965@gmail.com>
2025-12-21 01:57:23 +0900

    feat: 토큰 재발급 요청, 응답 DTO(#1)

---
+++ VoiceBook/src/main/java/com/sj/voicebook/member/dto/api/RefreshTokenRequest.java
@@ -0,0 +1,8 @@
+package com.sj.voicebook.member.dto.api;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RefreshTokenRequest(@NotBlank(message = "리프레시 토큰은 필수입니다.")
+                                  String refreshToken) {
+
+}
---
+++ VoiceBook/src/main/java/com/sj/voicebook/member/dto/api/RefreshTokenResponse.java
@@ -0,0 +1,4 @@
+package com.sj.voicebook.member.dto.api;
+
+public record RefreshTokenResponse(String accessToken, String refreshToken) {
+}
