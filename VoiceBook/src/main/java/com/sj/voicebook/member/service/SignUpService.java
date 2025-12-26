8503d355d6707eeb38d3bb451a36f264b903c25f
moonseungju <msj9965@gmail.com>
2025-12-26 21:17:28 +0900

    refactor: 회원가입 로직 반환값 변경 (#3)

--- VoiceBook/src/main/java/com/sj/voicebook/member/service/SignUpService.java
+++ VoiceBook/src/main/java/com/sj/voicebook/member/service/SignUpService.java
@@ -3,7 +3,7 @@ package com.sj.voicebook.member.service;
 import com.sj.voicebook.member.dto.application.CreateUserCommand;
 
 public interface SignUpService {
-    String signUp(CreateUserCommand command);
+    Long signUp(CreateUserCommand command);
     Boolean checkEmailDuplication(String email);
     Boolean checkNicknameDuplication(String nickname);
 
