4e38060924d0fc3b45ae41d24bdfd59050f0d77f
moonseungju <msj9965@gmail.com>
2025-12-26 21:52:20 +0900

    refactor: Command 객체 이름 변경 (#3)

--- VoiceBook/src/main/java/com/sj/voicebook/member/service/SignUpService.java
+++ VoiceBook/src/main/java/com/sj/voicebook/member/service/SignUpService.java
@@ -1,9 +1,9 @@
 package com.sj.voicebook.member.service;
 
-import com.sj.voicebook.member.dto.application.CreateUserCommand;
+import com.sj.voicebook.member.dto.application.SignUpCommand;
 
 public interface SignUpService {
-    Long signUp(CreateUserCommand command);
+    Long signUp(SignUpCommand command);
     Boolean checkEmailDuplication(String email);
     Boolean checkNicknameDuplication(String nickname);
 
