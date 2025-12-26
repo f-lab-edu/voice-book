3a880086218cb872e7e178264ba1398b671388ab
moonseungju <msj9965@gmail.com>
2025-12-26 20:54:03 +0900

    refactor: 넥네임 중복 체크 Validator 생성 및 적용(#3)

--- VoiceBook/src/main/java/com/sj/voicebook/member/service/impl/SignUpServiceImpl.java
+++ VoiceBook/src/main/java/com/sj/voicebook/member/service/impl/SignUpServiceImpl.java
@@ -5,8 +5,9 @@ import com.sj.voicebook.global.exception.ErrorCode;
 import com.sj.voicebook.member.domain.Member;
 import com.sj.voicebook.member.dto.application.CreateUserCommand;
 import com.sj.voicebook.member.repository.MemberRepository;
-import com.sj.voicebook.member.service.EmailDuplicationValidator;
+import com.sj.voicebook.member.service.validator.EmailDuplicationValidator;
 import com.sj.voicebook.member.service.SignUpService;
+import com.sj.voicebook.member.service.validator.NicknameDuplicatationValidtor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,6 +21,8 @@ public class SignUpServiceImpl implements SignUpService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final EmailDuplicationValidator emailDuplicationValidator;
+    private final NicknameDuplicatationValidtor nicknameDuplicatationValidtor;
+
     @Override
     @Transactional
     public String signUp(CreateUserCommand command) {
@@ -29,7 +32,7 @@ public class SignUpServiceImpl implements SignUpService {
         }
 
         // 닉네임 중복 검사
-        if (checkNicknameDuplication(command.nickname())) {
+        if (nicknameDuplicatationValidtor.isNicknameDuplicated(command.nickname())) {
             throw new BusinessException(ErrorCode.NICKNAME_DUPLICATION);
         }
 
@@ -52,6 +55,6 @@ public class SignUpServiceImpl implements SignUpService {
     @Override
     @Transactional(readOnly = true)
     public Boolean checkNicknameDuplication(String nickname) {
-        return memberRepository.existsByNickname(nickname);
+        return nicknameDuplicatationValidtor.isNicknameDuplicated(nickname);
     }
 }
---
+++ VoiceBook/src/main/java/com/sj/voicebook/member/service/validator/NicknameDuplicatationValidtor.java
@@ -0,0 +1,22 @@
+package com.sj.voicebook.member.service.validator;
+
+import com.sj.voicebook.global.exception.BusinessException;
+import com.sj.voicebook.global.exception.ErrorCode;
+import com.sj.voicebook.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+@RequiredArgsConstructor
+public class NicknameDuplicatationValidtor {
+    private final MemberRepository memberRepository;
+
+    public boolean isNicknameDuplicated(String nickname) {
+        if(!StringUtils.hasText(nickname)){
+            throw new BusinessException(ErrorCode.NOT_OR_NULL_INPUT_REQUIRED);
+        }
+        return memberRepository.existsByNickname(nickname);
+    }
+
+}
