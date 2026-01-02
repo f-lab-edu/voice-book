36b670675d34b59d0a3d74ec0a3165fde137733a
moonseungju <msj9965@gmail.com>
2026-01-02 17:14:46 +0900

    refactor: EmailDuplicationValidator에 읽기 전용 트랜잭션 설정 추가

--- VoiceBook/src/main/java/com/sj/voicebook/member/service/validator/EmailDuplicationValidator.java
+++ VoiceBook/src/main/java/com/sj/voicebook/member/service/validator/EmailDuplicationValidator.java
@@ -5,10 +5,12 @@ import com.sj.voicebook.global.exception.ErrorCode;
 import com.sj.voicebook.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
 @Component
+@Transactional(readOnly = true)
 public class EmailDuplicationValidator {
     private final MemberRepository memberRepository;
 
